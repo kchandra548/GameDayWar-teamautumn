28d772ccbc5591ce00bd956379ddd52fe7886aeb
bosep512 <bosep@microsoft.com>
2024-12-05 16:22:19 +0530

    Update GetProductByName.cs

--- IndividualChallenges/GetProductByName.cs
+++ IndividualChallenges/GetProductByName.cs
@@ -4,7 +4,9 @@ public void GetProductByName(string productName)
     string query = $"SELECT * FROM Products WHERE Name = '{productName}'";
     using (var context = new YourDbContext())
     {
-        var products = context.Database.SqlQuery<Product>(query).ToList();
+         var products = context.Products
+                              .Where(p => p.Name == productName)
+                              .ToList();
         foreach (var product in products)
         {
             Console.WriteLine($"Product: {product.Name}");
