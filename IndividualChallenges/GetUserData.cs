5032c28f238797e1836174a480d39e9819274718
bosep512 <bosep@microsoft.com>
2024-12-05 16:24:27 +0530

    Update GetUserData.cs

--- IndividualChallenges/GetUserData.cs
+++ IndividualChallenges/GetUserData.cs
@@ -1,14 +1,16 @@
 public void GetUserData(int id)
 {
-    // SQL injection vulnerability
     string connectionString = "YourConnectionStringHere";
-    string query = $"SELECT * FROM Users WHERE id={id}";
+    string query = "SELECT * FROM Users WHERE id = @Id";
 
     using (var connection = new SqlConnection(connectionString))
     {
         connection.Open();
         using (var command = new SqlCommand(query, connection))
         {
+            // Add parameter to the query
+            command.Parameters.AddWithValue("@Id", id);
+
             using (var reader = command.ExecuteReader())
             {
                 while (reader.Read())
